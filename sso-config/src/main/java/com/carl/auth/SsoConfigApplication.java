00e55c5eec401e3954e140c6638c07a050a5e4f4
袁继康 <jikang.yuan@bangdao-tech.com>
2019-04-02 11:35:09 +0800

    给cas服务器添加配置中心

---
+++ sso-config/src/main/java/com/carl/auth/SsoConfigApplication.java
@@ -0,0 +1,15 @@
+package com.carl.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SsoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoConfigApplication.class, args);
+	}
+
+}
